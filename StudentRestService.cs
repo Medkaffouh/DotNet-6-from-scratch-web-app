8d8231225d22b118c2195e953546c233ee51e922
Medkaffouh <kaffouhmohamed@gmail.com>
2022-08-11 22:01:00 +0100

    dotnet 6 web rest api app

--- StudentRestService.cs
+++ StudentRestService.cs
@@ -15,14 +15,29 @@ namespace HelloWorldProject2{
             return dbService.students;
         }
 
-        [HttpGet("Id")]
+        [HttpGet("{Id}")]
         public Student GetOne(long Id){
-            return dbService.students.FirstOrDefault(s =>s.Id==Id);
+            return dbService.students.FirstOrDefault(s=>s.Id==Id);
         }
 
         [HttpPost]
         public Student Save([FromBody]Student student){
             dbService.students.Add(student);
+            dbService.SaveChanges();
+            return student;
+        }
+
+        [HttpDelete("{Id}")]
+        public void Delete(long Id){
+            Student student = dbService.students.FirstOrDefault(s=>s.Id==Id);
+            dbService.students.Remove(student);
+            dbService.SaveChanges();
+        }
+
+        [HttpPut("{Id}")]
+        public Student Update(long Id,[FromBody]Student student){
+            student.Id=Id;
+            dbService.students.Update(student);
             dbService.SaveChanges();
             return student;
         }
